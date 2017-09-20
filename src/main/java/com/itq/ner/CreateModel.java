937d214de2d9208be64bbd71ce86e5703ffe4257
Riva'i Amin <raysaber37@gmail.com>
2017-09-20 14:37:38 +0700

    Person, Location, Thing Model

--- src/main/java/com/itq/ner/CreateModel.java
+++ src/main/java/com/itq/ner/CreateModel.java
@@ -24,35 +24,38 @@ public class CreateModel {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
+		
+		String modelType = args[0];
+
 		/**
 	     * establish a file to put sentences in
 	     */
-	    File sentences = new File("person/sentences.txt");
+	    File sentences = new File("classes/models/"+modelType+"/sentences.txt");
 
 	    /**
 	     * establish a file to put your NER hits in (the ones you want to keep based on prob)
 	     */
-	    File knownEntities = new File("person/knownentities.txt");
+	    File knownEntities = new File("classes/models/"+modelType+"/knownentities.txt");
 
 	    /**
 	     * establish a BLACKLIST file to put your bad NER hits in (also can be based on prob)
 	     */
-	    File blacklistedentities = new File("person/blentities.txt");
+	    File blacklistedentities = new File("classes/models/"+modelType+"/blentities.txt");
 
 	    /**
 	     * establish a file to write your annotated sentences to
 	     */
-	    File annotatedSentences = new File("person/annotatedSentences.txt");
+	    File annotatedSentences = new File("classes/models/"+modelType+"/annotatedSentences.txt");
 
 	    /**
 	     * establish a file to write your model to
 	     */
-	    File theModel = new File("person/colorModel.bin");
+	    File theModel = new File("classes/models/"+modelType+"/itq-"+modelType+"-model.bin");
 
 	    /**
 	     * THIS IS WHERE THE ADDON IS GOING TO USE THE FILES (AS IS) TO CREATE A NEW MODEL. YOU SHOULD NOT HAVE TO RUN THE FIRST PART AGAIN AFTER THIS RUNS, JUST NOW PLAY WITH THE
 	     * KNOWN ENTITIES AND BLACKLIST FILES AND RUN THE METHOD BELOW AGAIN UNTIL YOU GET SOME DECENT RESULTS (A DECENT MODEL OUT OF IT).
 	     */
-	    DefaultModelBuilderUtil.generateModel(sentences, knownEntities, blacklistedentities, theModel, annotatedSentences, "color", 3);
+	    DefaultModelBuilderUtil.generateModel(sentences, knownEntities, blacklistedentities, theModel, annotatedSentences, modelType, 3);
 	}
 }
